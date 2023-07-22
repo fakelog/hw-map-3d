e60f85069f2b8698068dbf2ea023dd308575e234
fakelog <solodyankin.d@outlook.com>
2023-07-22 09:10:35 +0500

    Merge remote branch 'bugfix/not-updating-graph'

--- src/lib/components/RouteOverlay.svelte
+++ src/lib/components/RouteOverlay.svelte
@@ -1,45 +1,43 @@
 <script>
-    import { loadData } from "$lib/stores/localStroage";
+  import Close from "./icons/Close.svelte";
+  import IconButton from "./IconButton.svelte";
+  import Input from "./Input.svelte";
+  import { connectionsStore } from "$lib/stores/connections";
 
-    import Close from "./icons/Close.svelte";
-    import IconButton from "./IconButton.svelte";
-    import Input from "./Input.svelte";
-    import { writable } from "svelte/store";
+  function onClickAdd() {
+    connectionsStore.update((arr) => [...arr, ""]);
+  }
 
-    let connections = writable(loadData("connections"));
-    $: connectionsData = $connections;
+  function onClickDelete(index) {
+    connectionsStore.update((arr) => arr.filter((_, i) => i !== index));
+  }
 
-    function onClickAdd() {
-        connections.update((arr) => [...arr, ""]);
-    }
+  function handleChange(event, index) {
+    connectionsStore.update((arr) => {
+      const updatedArr = [...arr];
+      updatedArr[index] = event.detail;
 
-    function onClickDelete(index) {
-        connections.update((arr) => arr.filter((_, i) => i !== index));
-    }
+      const _arr = updatedArr.slice();
 
-    function handleChange(event, index) {
-        connections.update((arr) => {
-            const updatedArr = [...arr];
-            updatedArr[index] = event.detail;
-            return updatedArr;
-        });
-    }
+      return _arr;
+    });
+  }
 </script>
 
 <div
-    class="sticky z-20 w-fit h-fit m-4 bg-opacity-60 rounded-xl p-2 bg-neutral-600 backdrop-blur-sm flex flex-col"
+  class="sticky z-20 w-fit h-fit m-4 bg-opacity-60 rounded-xl p-2 bg-neutral-600 backdrop-blur-sm flex flex-col"
 >
-    {#each connectionsData as connection, index}
-        <div class="flex items-center m-2 p-2 space-x-2">
-            <Input
-                value={connection}
-                on:input={(event) => handleChange(event, index)}
-            />
-            <IconButton on:click={() => onClickDelete(index)}>
-                <Close />
-            </IconButton>
-        </div>
-    {/each}
+  {#each $connectionsStore as connection, index}
+    <div class="flex items-center m-2 p-2 space-x-2">
+      <Input
+        value={connection}
+        on:input={(event) => handleChange(event, index)}
+      />
+      <IconButton on:click={() => onClickDelete(index)}>
+        <Close />
+      </IconButton>
+    </div>
+  {/each}
 
-    <IconButton on:click={onClickAdd}>+</IconButton>
+  <IconButton on:click={onClickAdd}>+</IconButton>
 </div>
