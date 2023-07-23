b39782f048e1686faca129e3d5e50ef7eaf5cd7c
fakelog <solodyankin.d@outlook.com>
2023-07-23 11:42:53 +0500

    graph to component

---
+++ src/lib/components/ForceGraph3D.svelte
@@ -0,0 +1,89 @@
+<script>
+    import { onMount } from "svelte";
+
+    import SpriteText from "three-spritetext";
+    import ForceGraph3D from "3d-force-graph";
+    import { connectionsStore } from "$lib/stores/connections";
+
+    let graphElement;
+    let nodes = [];
+    let links = [];
+    let data = {};
+
+    const GraphInstance = ForceGraph3D();
+
+    function changeGroupById(nodes, id, newGroup) {
+        const node = nodes.find((node) => node.id === id);
+
+        if (node) {
+            node.group = newGroup;
+        }
+    }
+
+    nodes = Array.from({ length: 256 }, (_, i) => {
+        const hexValue = i.toString(16).toUpperCase().padStart(2, "0");
+        return {
+            id: hexValue,
+            name: hexValue,
+            group: 1,
+        };
+    });
+
+    $: {
+        const _links = [];
+        const _nodes = [...nodes];
+
+        $connectionsStore.forEach((connection) => {
+            const nodesInConnection = connection
+                .split(" > ")
+                .filter(Boolean)
+                .filter((value) => value.length === 2);
+
+            nodesInConnection.forEach((nodeId, index) => {
+                changeGroupById(_nodes, nodeId, index);
+                if (index < nodesInConnection.length - 1) {
+                    _links.push({
+                        source: nodeId,
+                        target: nodesInConnection[index + 1],
+                    });
+                }
+            });
+        });
+
+        links = _links;
+        nodes = _nodes;
+
+        data = { nodes, links };
+
+        GraphInstance.graphData(data)
+            .nodeAutoColorBy("group")
+            .nodeThreeObject((node) => {
+                const sprite = new SpriteText(node.id);
+                sprite.material.depthWrite = true;
+                sprite.color = node.color;
+                sprite.textHeight = 8;
+                return sprite;
+            });
+    }
+
+    onMount(() => {
+        if (!graphElement) {
+            console.error("ooops");
+            return;
+        }
+
+        GraphInstance(graphElement)
+            .backgroundColor("#000")
+            .graphData(data)
+            .nodeAutoColorBy("group")
+            .nodeThreeObject((node) => {
+                const sprite = new SpriteText(node.id);
+                sprite.material.depthWrite = true;
+                sprite.color = node.color;
+                sprite.textHeight = 8;
+                return sprite;
+            });
+    });
+</script>
+
+<div bind:this={graphElement} class="top-0 absolute" />
