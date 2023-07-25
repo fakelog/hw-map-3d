1e49df5b86b91aefc656d1268304770272ea8107
fakelog <solodyankin.d@outlook.com>
2023-07-25 13:09:57 +0500

    RouteOverlay: add overflow

--- src/lib/components/RouteOverlay.svelte
+++ src/lib/components/RouteOverlay.svelte
@@ -28,17 +28,19 @@
 <div
   class="flex flex-col w-full h-auto bg-opacity-60 rounded-xl p-4 space-y-4 bg-background backdrop-blur-sm"
 >
-  {#each $connectionsStore as connection, index}
-    <div class="flex items-center space-x-2">
-      <Input
-        value={connection}
-        on:change={(event) => onChangeInput(event, index)}
-      />
-      <IconButton on:click={() => onClickDelete(index)}>
-        <Delete />
-      </IconButton>
-    </div>
-  {/each}
+  <div class="space-y-4 max-h-96 overflow-auto">
+    {#each $connectionsStore as connection, index}
+      <div class="flex items-center space-x-2">
+        <Input
+          value={connection}
+          on:change={(event) => onChangeInput(event, index)}
+        />
+        <IconButton on:click={() => onClickDelete(index)}>
+          <Delete />
+        </IconButton>
+      </div>
+    {/each}
+  </div>
 
   <IconButton on:click={onClickAdd}
     ><span class="text-accent">+</span></IconButton
