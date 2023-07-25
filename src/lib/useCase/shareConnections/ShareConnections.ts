a5ecbe9b4130c03b87979b19339e1d590a02c157
fakelog <solodyankin.d@outlook.com>
2023-07-25 15:55:45 +0500

    return string

--- src/lib/useCase/shareConnections/ShareConnections.ts
+++ src/lib/useCase/shareConnections/ShareConnections.ts
@@ -2,16 +2,17 @@ import { connectionsStore } from "$lib/stores/connections";
 import { ConnectionsUtils } from "$lib/utils/ConnectionsUtils"
 
 
-export async function getShareConnections(): Promise<Array<string[]>> {
+export async function getShareConnections(): Promise<string> {
 
     try {
         const connectionsUtils = new ConnectionsUtils();
 
         // Subscribe to the connectionsStore using Promise to wait for the initial value
-        const connections: Array<string[]> = await new Promise((resolve) => {
+        const connections: string = await new Promise((resolve) => {
             const unsubscribe = connectionsStore.subscribe((value) => {
                 const cleanedUpConnections = connectionsUtils.getCleanupConnections(value);
-                resolve(cleanedUpConnections);
+                const jsonString = JSON.stringify(cleanedUpConnections);
+                resolve(jsonString);
                 unsubscribe(); // Unsubscribe after getting the initial value
             });
         });
@@ -19,6 +20,6 @@ export async function getShareConnections(): Promise<Array<string[]>> {
         return connections;
     } catch (error) {
         console.error("Error while getting share connections:", error);
-        return [];
+        return "";
     }
 }
