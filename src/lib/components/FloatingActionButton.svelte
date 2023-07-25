48315ab29d92f40aedb93bca45a168549c30d0ae
fakelog <solodyankin.d@outlook.com>
2023-07-25 12:26:17 +0500

    add title in button

--- src/lib/components/FloatingActionButton.svelte
+++ src/lib/components/FloatingActionButton.svelte
@@ -3,6 +3,10 @@
 
     import IconButton from "./IconButton.svelte";
 
+    let title = "FloatingActionButton";
+
+    export { title };
+
     const dispatch = createEventDispatcher();
 
     function onClicked(event: CustomEvent<any>) {
@@ -11,7 +15,7 @@
 </script>
 
 <div class="rounded-xl bg-background bg-opacity-60 backdrop-blur-sm">
-    <IconButton class="p-4" on:click={onClicked}>
+    <IconButton class="p-4" {title} on:click={onClicked}>
         <slot />
     </IconButton>
 </div>
--- src/lib/components/IconButton.svelte
+++ src/lib/components/IconButton.svelte
@@ -2,7 +2,10 @@
     import { createEventDispatcher } from "svelte";
 
     let classExport = "";
+    let title = "";
+
     export { classExport as class };
+    export { title };
 
     if (classExport === "") {
         classExport = "p-2";
@@ -17,6 +20,7 @@
 
 <button
     class="rounded-lg transition-all duration-150 ease-linear hover:bg-amber-200 hover:bg-opacity-10 active:bg-gradient-radial from-[#96850520] to-transparent {classExport}"
+    {title}
     on:click={onClick}
 >
     <slot />
