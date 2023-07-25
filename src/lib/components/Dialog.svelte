22df1ec882ca8f262dd5d2f797d2d320b1d8f6db
fakelog <solodyankin.d@outlook.com>
2023-07-25 12:04:55 +0500

    add dialog component

---
+++ src/lib/components/Dialog.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    import { createEventDispatcher } from "svelte";
+
+    import Surface from "./Surface.svelte";
+    import TextButton from "./TextButton.svelte";
+    import Window from "./Window.svelte";
+
+    let title: string = "Dialog title";
+
+    const dispatch = createEventDispatcher();
+
+    function onClick(type: string) {
+        dispatch("click", type);
+    }
+</script>
+
+<Surface>
+    <Window>
+        <header
+            class="flex items-center justify-center w-full p-4 rounded-t-xl text-stone-300 bg-background bg-opacity-50"
+        >
+            {title}
+        </header>
+        <main class="p-4 text-stone-400"><slot /></main>
+        <footer class="p-2 rounded-b-xl bg-background bg-opacity-10">
+            <TextButton
+                text="Отмена"
+                type="negative"
+                on:click={() => {
+                    onClick("cancel");
+                }}
+            />
+            <TextButton
+                text="Готово"
+                on:click={() => {
+                    onClick("done");
+                }}
+            />
+        </footer>
+    </Window>
+</Surface>
