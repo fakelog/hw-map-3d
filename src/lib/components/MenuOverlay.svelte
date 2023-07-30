0e5f41d288380733182c6f241b4aed4ae401d01c
fakelog <solodyankin.d@outlook.com>
2023-07-30 23:38:51 +0500

    lib: components: MenuOverlay: use ShareFAB

--- src/lib/components/MenuOverlay.svelte
+++ src/lib/components/MenuOverlay.svelte
@@ -1,51 +1,13 @@
 <script lang="ts">
-    import { routesStore } from "$lib/stores/routes";
-    import { apiBaseURL } from "$lib/const/apiBaseURL";
-    import { loadData, saveData } from "$lib/stores/localStroage";
-
-    import Dialog from "./Dialog.svelte";
-    import FloatingActionButton from "./buttons/FloatingActionButton/FloatingActionButton.svelte";
     import RouteOverlay from "./RouteOverlay.svelte";
-    import Share from "./icons/Share.svelte";
     import SaveFloatingActionButton from "./buttons/FloatingActionButton/SaveFloatingActionButton.svelte";
-
-    let showShareDialog = false;
-    let shareMessage: string = "";
-    let shareURL: string = "";
-
-    async function onClickShare() {
-        try {
-            shareMessage = JSON.stringify(loadData("connections"));
-        } catch (error: any) {
-            shareMessage = error;
-        }
-        try {
-            shareURL = JSON.stringify(loadData("connections"));
-        } catch (error: any) {
-            shareURL = error;
-        }
-        showShareDialog = true;
-    }
-
-    function onClickDialog() {
-        showShareDialog = false;
-    }
+    import ShareFloatingActionButton from "./buttons/FloatingActionButton/ShareFloatingActionButton.svelte";
 </script>
-
-{#if showShareDialog}
-    <Dialog title="Поделиться" on:click={onClickDialog}>
-        <textarea name="" id="" cols="30" rows="10">
-            {shareMessage}
-        </textarea>
-    </Dialog>
-{/if}
 
 <div class="flex flex-col w-full justify-between space-y-4">
     <RouteOverlay />
     <div class="flex flex-row justify-end space-x-4">
-        <FloatingActionButton title="Поделиться" on:click={onClickShare}>
-            <Share />
-        </FloatingActionButton>
+        <ShareFloatingActionButton />
         <SaveFloatingActionButton />
     </div>
 </div>
