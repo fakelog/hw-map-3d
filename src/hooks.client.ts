7ea3b3339325dfe5fea1749670028500bb9d2619
fakelog <solodyankin.d@outlook.com>
2023-07-30 23:49:17 +0500

    hooks: use default routes if not exist

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,9 +1,15 @@
 import { loadData, removeData, saveData } from "$lib/stores/localStroage";
+import { defaultConnections } from "$lib/const/defaultConnections";
 
-const connections = loadData("connections");
+const connections = loadData('connections');
 if (connections) {
     if (connections.length > 0) {
-        saveData("routes", connections);
+        saveData('routes', connections);
     }
     removeData("connections");
+}
+
+const routes = loadData('routes');
+if (!routes) {
+    saveData('routes', defaultConnections);
 }
