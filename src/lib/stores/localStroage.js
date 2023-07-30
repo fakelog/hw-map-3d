8fac3d18c4fa1eb986d9bcfd6419110ef6bd5f92
fakelog <solodyankin.d@outlook.com>
2023-07-30 18:16:23 +0500

    lib: stores: localStroage: add funct remove data

--- src/lib/stores/localStroage.js
+++ src/lib/stores/localStroage.js
@@ -1,8 +1,15 @@
-export function saveData(key, data) {
-    localStorage.setItem(key, JSON.stringify(data));
-}
-
-export function loadData(key) {
+function loadData(key) {
     const data = localStorage.getItem(key);
     return JSON.parse(data);
 }
+
+function removeData(key) {
+    const data = localStorage.removeItem(key)
+    return JSON.parse(data);
+}
+
+function saveData(key, data) {
+    localStorage.setItem(key, JSON.stringify(data));
+}
+
+export { loadData, removeData, saveData }
